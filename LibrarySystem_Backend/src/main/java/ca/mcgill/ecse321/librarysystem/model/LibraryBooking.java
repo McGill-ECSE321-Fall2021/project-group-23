60fae34ee51c009e0c19685496f49eaf13d88338
Abdoou213 <78567980+Abdoou213@users.noreply.github.com>
2021-10-17 13:25:11 -0400

    model LibraryBooking and Reservation

--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/LibraryBooking.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/LibraryBooking.java
@@ -2,7 +2,63 @@ package ca.mcgill.ecse321.librarysystem.model;
 import java.sql.Date;
 import java.sql.Time;
 
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class LibraryBooking
 {
 
+    private int id;
+
+    @Id
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int newId) {
+        this.id = newId;
+    }
+
+    private Date startDate;
+
+    public Date getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(Date newDate) {
+        this.startDate = newDate;
+    }
+    private Date endDate;
+
+    public Date getEndDate() {
+        return this.endDate;
+    }
+
+    public void setEndDate(Date newDate) {
+        this.endDate = newDate;
+    }
+    private Time startTime;
+
+    public Time getStartTime() {
+        return this.startTime;
+    }
+
+    public void setStartTime(Time newTime) {
+        this.startTime = newTime;
+    }
+    private Time endTime;
+    public Time getEndTime() {
+        return this.endTime;
+    }
+
+    public void setEndTime(Time newTime) {
+        this.endTime = newTime;
+    }
+
+    private User user;
+
+    @ManyToOne(optional = false)
+    public User getUser() {
+        return this.user;
+    }
 }
--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Reservation.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Reservation.java
@@ -1,7 +1,70 @@
 package ca.mcgill.ecse321.librarysystem.model;
 import java.sql.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Reservation
 {
-
+	private int id;
+	
+	@Id
+	public int getid() {
+		return this.id;
+	}
+	public void setId(int newId) {
+		this.id = newId;
+	}
+	private Date reservationStartDate;
+	
+	public Date getReservationStartDate() {
+		return this.reservationStartDate;
+	}
+	public void setReservationStartDate(Date startDate) {
+		this.reservationStartDate = startDate;
+	}
+	
+	private Date reservationEndDate;
+	
+	public Date getReservationEndDate() {
+		return this.reservationEndDate;
+	}
+	public void setReservationEndDate(Date endDate) {
+		this.reservationEndDate = endDate;
+	}
+	private boolean isCheckedOut;
+	
+	public boolean getIsCheckedOut() {
+		return this.isCheckedOut;
+	}
+	
+	public void setIsCheckedOut(boolean checkout) {
+		this.isCheckedOut = checkout;
+	}
+	private Item item;
+	
+	@OneToMany
+	public Item getItem() {
+		return this.item;
+	}
+	
+	
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	
+	private User user;
+	
+	@ManyToOne(optional = false)
+	public User getUser() {
+		return this.user;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 }
