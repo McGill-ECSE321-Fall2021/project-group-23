8d64e7321da6263933fc0effc86ba39883078660
Capt0bvious <abiola.olaniyan15@gmail.com>
2021-10-17 13:21:22 -0400

    Added attributes and associations for librarian, shift and schedule classes

--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Librarian.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Librarian.java
@@ -1,7 +1,19 @@
 package ca.mcgill.ecse321.librarysystem.model;
 import java.util.*;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Librarian extends Account
 {
-
+	private WeeklySchedule librarianSchedule;
+	@Id
+	public WeeklySchedule getLibrarianSchedule() {
+		return this.librarianSchedule;
+	}
+	public void setLibrarianSchedule(WeeklySchedule newSchedule) {
+		this.librarianSchedule = newSchedule;
+	}
+	
 }
--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/WeeklySchedule.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/WeeklySchedule.java
@@ -1,8 +1,31 @@
 package ca.mcgill.ecse321.librarysystem.model;
 import java.util.*;
-import java.sql.Time;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+
+@Entity
 public class WeeklySchedule
 {
-
+	private Set<Shift> librarianShifts;
+	
+	@OneToMany(cascade={CascadeType.ALL})
+	public Set<Shift> getLibrarianShifts() {
+		return this.librarianShifts;
+	}
+	
+	public void setLibrarianShifts(Set<Shift> newShifts) {
+		this.librarianShifts = newShifts;
+	}
+	
+	private Librarian employee;
+	@Id
+	public Librarian getEmployee() {
+		return this.employee;
+	}
+	public void setEmployee(Librarian newEmployee) {
+		this.employee = newEmployee;
+	}
 }
