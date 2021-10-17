ee65d68c009745324d40ed3e93a68df4a168bb38
Eric Zhang <eric.zhang3@mail.mcgill.ca>
2021-10-17 12:26:18 -0400

    Added model classes for Holiday and OpeningHours

--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Holiday.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/model/Holiday.java
@@ -1,7 +1,31 @@
 package ca.mcgill.ecse321.librarysystem.model;
 import java.sql.Date;
 
-public class Holiday
-{
+@Entity
+public class Holiday{ 
 
+	private String name;
+	public void setName(String value) {
+		this.name = value;
+	}
+@Id
+	public String getName() {
+		return this.name;
+	}
+
+	private Date startDate;
+	public void setStartDate(Date value) {
+		this.startDate = value;
+	}
+	public Date getStartDate() {
+		return this.startDate;
+	}
+
+	private Date endDate;
+	public void setEndDate(Date value) {
+		this.endDate = value;
+	}
+	public Date getEndDate() {
+		return this.endDate;
+	}
 }
