71f8fb1f8ddf9b9e1acea5cd7deb1fd2743a3e74
Bashar Eskandar <basharo9999@hotmail.com>
2021-10-18 14:45:20 -0400

    CRUD repo and tests for library done

--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/LibraryRepository.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/LibraryRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.librarysystem.model.Library;
 
 public interface LibraryRepository extends CrudRepository<Library, Integer> {
-    
+    Library findByName(String name);
+    boolean existsByName(String name);
 }
