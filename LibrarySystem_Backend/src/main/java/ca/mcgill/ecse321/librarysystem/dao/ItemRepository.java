90c89c35794ff779f932055265fe2f0b71aedb59
JoelLussier <71228556+JoelLussier@users.noreply.github.com>
2021-10-17 21:57:32 -0400

    Implemented percistence tests for Items

--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/ItemRepository.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/dao/ItemRepository.java
@@ -1,11 +1,32 @@
 package ca.mcgill.ecse321.librarysystem.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import ca.mcgill.ecse321.librarysystem.model.Item;
 
 public interface ItemRepository extends CrudRepository<Item, Integer>{
-
+/**
+     * Finds an item using the Item Id (unique)
+     * @param id
+     * @return
+     */
     Item findItemByItemId(int id);
-    
+
+
+    /**
+     * Searches for the item by name 
+     * Returns a list<Item> since name is not unique
+     * @param Name
+     * @return
+     */
+    List<Item> findItemByName(String Name);
+
+    /**
+     * Deletes an item using the Item Id
+     * @param id
+     * @return
+     */
+    void deleteByItemId(int id);
 }
--- LibrarySystem_Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestItemsPersistence.java
+++ LibrarySystem_Backend/src/test/java/ca/mcgill/ecse321/librarysystem/dao/TestItemsPersistence.java
@@ -1,11 +1,143 @@
 package ca.mcgill.ecse321.librarysystem.dao;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import ca.mcgill.ecse321.librarysystem.model.Album;
+import ca.mcgill.ecse321.librarysystem.model.Archive;
+import ca.mcgill.ecse321.librarysystem.model.Book;
+import ca.mcgill.ecse321.librarysystem.model.Movie;
+import ca.mcgill.ecse321.librarysystem.model.Newspapers;
+import ca.mcgill.ecse321.librarysystem.model.Item.Status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class TestItemsPersistence {
-    
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @BeforeEach
+	public void clearDatabase() {
+		
+		itemRepository.deleteAll();
+		
+	}
+
+
+    @Test
+    public void testPersistAndLoadBook() {
+        
+        int id = 000000000;
+        String name = "Lord Of The Rings";
+        Status status = Book.Status.AVAILABLE;
+        Book book = new Book();
+        book.setTitle(name);
+        book.setStatus(status);
+        book.setItemId(id);
+        itemRepository.save(book);
+
+        book = null;
+
+        book = (Book) itemRepository.findItemByItemId(id);
+        assertNotNull(book);
+        assertEquals(id, book.getItemId());
+        
+
+    }
+
+    @Test
+    public void testPersistAndLoadMovie() {
+        
+        int id = 000000000;
+        String name = "Lord Of The Rings";
+        Status status = Movie.Status.AVAILABLE;
+        Movie movie = new Movie();
+        movie.setTitle(name);
+        movie.setStatus(status);
+        movie.setItemId(id);
+        itemRepository.save(movie);
+
+        movie = null;
+
+        movie = (Movie) itemRepository.findItemByItemId(id);
+        assertNotNull(movie);
+        assertEquals(id, movie.getItemId());
+        
+
+    }
+
+    @Test
+    public void testPersistAndLoadAlbum() {
+        
+        int id = 000000000;
+        String name = "OK Computer";
+        Status status = Album.Status.AVAILABLE;
+        Album album = new Album();
+        album.setTitle(name);
+        album.setStatus(status);
+        album.setItemId(id);
+        itemRepository.save(album);
+
+        album = null;
+
+        album = (Album) itemRepository.findItemByItemId(id);
+        assertNotNull(album);
+        assertEquals(id, album.getItemId());
+        
+
+    }
+
+    @Test
+    public void testPersistAndLoadNewspaper() {
+        
+        int id = 000000000;
+        String name = "La Presse";
+        Status status = Newspapers.Status.AVAILABLE;
+        Newspapers newspapers = new Newspapers();
+        newspapers.setTitle(name);
+        newspapers.setStatus(status);
+        newspapers.setItemId(id);
+        itemRepository.save(newspapers);
+
+        newspapers = null;
+
+        newspapers = (Newspapers) itemRepository.findItemByItemId(id);
+        assertNotNull(newspapers);
+        assertEquals(id, newspapers.getItemId());
+        
+
+    }
+
+    @Test
+    public void testPersistAndLoadArchive() {
+        
+        int id = 000000000;
+        String name = "Encyclopedia Vol.3";
+        Status status = Archive.Status.AVAILABLE;
+        Archive archive = new Archive();
+        archive.setTitle(name);
+        archive.setStatus(status);
+        archive.setItemId(id);
+        itemRepository.save(archive);
+
+        archive = null;
+
+        archive = (Archive) itemRepository.findItemByItemId(id);
+        assertNotNull(archive);
+        assertEquals(id, archive.getItemId());
+        
+
+    }
+
+
 }
