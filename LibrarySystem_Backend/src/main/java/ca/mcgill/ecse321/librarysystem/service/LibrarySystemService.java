1ebce2a5e7f52288c819671b68758d586bdd38d8
Bashar Eskandar <basharo9999@hotmail.com>
2021-11-05 17:46:41 -0400

    created controller dto srvice packages

---
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/LibrarySystemService.java
@@ -0,0 +1,16 @@
+package ca.mcgill.ecse321.librarysystem.service;
+import java.sql.Date;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.librarysystem.*;
+
+@Service
+public class LibrarySystemService {
+    
+}
