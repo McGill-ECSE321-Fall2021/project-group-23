d248693c8512dfb9eb026041e155b431adea7562
zichzhang <zi.c.zhang@mail.mcgill.ca>
2021-11-07 16:55:22 -0500

    Added updateCustomer service method in LibrarianService class

--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/LibrarianService.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/LibrarianService.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ca.mcgill.ecse321.librarysystem.dao.CustomerRepository;
 import ca.mcgill.ecse321.librarysystem.dao.LibrarianRepository;
 import ca.mcgill.ecse321.librarysystem.dao.WeeklyScheduleRepository;
+import ca.mcgill.ecse321.librarysystem.model.Customer;
 import ca.mcgill.ecse321.librarysystem.model.Librarian;
 import ca.mcgill.ecse321.librarysystem.model.WeeklySchedule;
 
@@ -18,6 +20,8 @@ public class LibrarianService {
 	LibrarianRepository librarianRepository;
 	@Autowired
 	WeeklyScheduleRepository weeklyScheduleRepository;
+	@Autowired
+  CustomerRepository customerRepository;
 	
 	@Transactional
 	public Librarian createLibrarian(String firstName, String lastName, String password, int newScheduleId) {
@@ -120,6 +124,29 @@ public class LibrarianService {
 	public List<Librarian> getAllLibrarians() {
 		return toList(librarianRepository.findAll());
 	}
+
+	@Transactional
+  public Customer updateCustomer(int id, boolean newIsVerified, boolean newIsLocal, int newBalance) {
+    if (String.valueOf(id).length() == 0) {
+      throw new IllegalArgumentException("Customer id cannot be empty.");
+    } 
+    else {
+      Customer customer = customerRepository.findCustomerByAccountId(id);
+      if (customer == null) {
+        throw new IllegalArgumentException("The customer with provided id cannot be found.");
+			}
+			if (String.valueOf(newBalance).length() == 0 || newBalance < 0) {
+				throw new IllegalArgumentException("The provided account balance cannot be empty or negative.");
+			}
+      else {
+        customer.setIsVerified(newIsVerified);
+        customer.setIsLocal(newIsLocal);
+				customer.setAccountBalance(newBalance);
+        customerRepository.save(customer);
+        return customer;
+      }
+    }
+  }
 	
 	private <T> List<T> toList(Iterable<T> iterable){
 		List<T> resultList = new ArrayList<T>();
