f46ea51c544028d8f01a86e6aed291c964c3b6de
Capt0bvious <abiola.olaniyan15@gmail.com>
2021-11-08 17:39:47 -0500

    Created HeadLibrarian controller and service, moved methods around

---
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/controller/HeadLibrarianRestController.java
@@ -0,0 +1,168 @@
+package ca.mcgill.ecse321.librarysystem.controller;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.librarysystem.dto.CustomerDto;
+import ca.mcgill.ecse321.librarysystem.dto.LibrarianDto;
+import ca.mcgill.ecse321.librarysystem.dto.ShiftDto;
+import ca.mcgill.ecse321.librarysystem.dto.WeeklyScheduleDto;
+import ca.mcgill.ecse321.librarysystem.model.Customer;
+import ca.mcgill.ecse321.librarysystem.model.Librarian;
+import ca.mcgill.ecse321.librarysystem.model.Shift;
+import ca.mcgill.ecse321.librarysystem.model.WeeklySchedule;
+import ca.mcgill.ecse321.librarysystem.service.HeadLibrarianService;
+
+@CrossOrigin(origins = "*")
+@RestController
+public class HeadLibrarianRestController {
+	@Autowired 
+	HeadLibrarianService headLibrarianService;
+	
+	// Return all current librarians in the database
+	@GetMapping(value = { "/getAllLibrarians", "/getAllLibrarians/" })
+	public List<LibrarianDto> getAllLibrarians() {
+		List<LibrarianDto> librarianDtos = new ArrayList<>();
+		for (Librarian librarian : headLibrarianService.getAllLibrarians()) {
+			librarianDtos.add(convertToDto(librarian));
+		}
+		return librarianDtos;
+	}
+	
+	// Return librarian matching id
+	@GetMapping(value = { "/getLibrarianById/{librarianId}", "getLibrarianById/{librarianId}/" })
+	public LibrarianDto getLibrarianById(@PathVariable("librarianId") int librarianId)
+	throws IllegalArgumentException{
+		return convertToDto(headLibrarianService.getLibrarianByAccountId(librarianId));
+	}
+	
+	// Return librarian matching names
+	@GetMapping(value = { "/getLibrarianByNames/{firstName}/{lastName}", "getLibrarianByNames/{firstName}/{lastName}/" })
+	public LibrarianDto getLibrarianByFirstAndLastName(@PathVariable("firstName") String firstName, @PathVariable("lastName") String lastName)
+	throws IllegalArgumentException{
+		return convertToDto(headLibrarianService.getLibrarianByNames(firstName, lastName));
+	}
+	
+	// Return librarian with associated schedule
+	@GetMapping(value = { "/getLibrarianBySchedule/{weeklyScheduleId}", "getLibrarianBySchedule/{weeklyScheduleId}/" })
+	public LibrarianDto getLibrarianByWeeklySchedule(@PathVariable("weeklyScheduleId") int weeklyScheduleId)
+	throws IllegalArgumentException{
+		return convertToDto(headLibrarianService.getLibrarianByScheduleId(weeklyScheduleId));
+	}
+	
+	// Create a permanent head librarian account
+	@PostMapping(value = { "/createHeadLibrarian/{firstName}/{lastName}/{password}/{scheduleId}", "/createHeadLibrarian/{firstName}/{lastName}/{password}/{scheduleId}/" })
+	public LibrarianDto createHeadLibrarian(@PathVariable("firstName") String firstName, 
+			@PathVariable("lastName") String lastName,
+			@PathVariable("password") String password,
+			@PathVariable("scheduleId") int scheduleId) throws IllegalArgumentException{
+		Librarian headLibrarian = headLibrarianService.createHeadLibrarian(firstName, lastName, password, scheduleId);
+		return convertToDto(headLibrarian);
+	}
+	
+	// Create a new librarian account
+	@PostMapping(value = { "/createLibrarian/{firstName}/{lastName}/{password}/{scheduleId}", "/createLibrarian/{firstName}/{lastName}/{password}/{scheduleId}/" })
+	public LibrarianDto createLibrarian(@PathVariable("firstName") String firstName, 
+			@PathVariable("lastName") String lastName,
+			@PathVariable("password") String password,
+			@PathVariable("scheduleId") int scheduleId) throws IllegalArgumentException{
+		Librarian librarian = headLibrarianService.createLibrarian(firstName, lastName, password, scheduleId);
+		return convertToDto(librarian);
+	}
+	
+	// Update the head librarian account
+	@PutMapping(value = { "/updateHeadLibrarian/{accountId}/{firstName}/{lastName}/{password}/{scheduleId}", "/updateHeadLibrarian/{accountId}/{firstName}/{lastName}/{password}/{scheduleId}/" })
+	public LibrarianDto updateHeadLibarian(@PathVariable("accountId") int accountId,
+			@PathVariable("firstName") String firstName, 
+			@PathVariable("lastName") String lastName,
+			@PathVariable("password") String password,
+			@PathVariable("scheduleId") int scheduleId) throws IllegalArgumentException{
+		Librarian headLibrarian = headLibrarianService.updateHeadLibrarian(accountId, firstName, lastName, password, scheduleId);
+		return convertToDto(headLibrarian);
+	}
+	
+	// Update the librarian's schedule
+	@PutMapping(value = { "/updateLibrarianSchedule/{librarianId}/{newScheduleId}", "/updateLibrarianSchedule/{librarianId}/{newScheduleId}/" })
+	public LibrarianDto updateLibrarianSchedule(@PathVariable("librarianId") int librarianId, @PathVariable("newScheduleId") int newScheduleId) {
+		Librarian librarian = headLibrarianService.assignWeeklySchedule(librarianId, newScheduleId);
+		return convertToDto(librarian);
+	}
+	
+	// Update the customer's verification status, and/or local status, and/or balance
+	@PutMapping(value= { "/updateCustomer/head/{id}/{newIsVerified}/{newIsLocal}/{newBalance}", "/updateCustomer/head/{id}/{newIsVerified}/{newIsLocal}/{newBalance}/" })
+	public CustomerDto updateCustomer(@PathVariable("id") int id, @PathVariable("newIsVerified") boolean newIsVerified, @PathVariable("newIsLocal") boolean newIsLocal, @PathVariable("newBalance") int newBalance) {
+		Customer customer = headLibrarianService.updateCustomer(id, newIsVerified, newIsLocal, newBalance);
+		return convertToDto(customer);
+	}
+	
+	// Delete a librarian
+	@DeleteMapping(value = { "/deleteLibrarian/{librarianId}", "/deleteLibrarian/{librarianId}/" }) 
+	public LibrarianDto deleteLibrarian(@PathVariable("librarianId") int librarianId) {
+		return convertToDto(headLibrarianService.deleteLibrarian(librarianId));
+	}
+	
+	// Delete all librarians in the system
+	@DeleteMapping(value = { "/deleteAllLibrarians", "deleteAllLibrarians" })
+	public List<LibrarianDto> deleteAllLibrarians() {
+		List<LibrarianDto> librarianDtos = new ArrayList<>();
+		for (Librarian librarian : headLibrarianService.deleteAllLibrarians()) {
+			LibrarianDto librarianDto = convertToDto(librarian);
+			librarianDtos.add(librarianDto);
+		}
+		return librarianDtos;
+	}
+	
+	private LibrarianDto convertToDto(Librarian l) {
+		if (l == null) {
+			throw new IllegalArgumentException("There is no such Librarian!");
+		}
+		LibrarianDto librarianDto = new LibrarianDto(l.getAccountId(), l.getFirstName(), l.getLastName(), l.getPassword(), convertToDto(l.getLibrarianSchedule()));
+		return librarianDto;
+	}
+	
+	private WeeklyScheduleDto convertToDto(WeeklySchedule ws) {
+		if (ws == null) {
+			throw new IllegalArgumentException("There is no such WeeklySchedule!");
+		}
+		Set<ShiftDto> shiftDtos = new HashSet<ShiftDto>();
+		for (Shift s : ws.getShifts()) {
+			shiftDtos.add(convertToDto(s));
+		}
+		WeeklyScheduleDto weeklyScheduleDto = new WeeklyScheduleDto(ws.getWeeklyScheduleId(), shiftDtos);
+		return weeklyScheduleDto;
+	}
+	
+	private ShiftDto convertToDto(Shift s) {
+		if (s == null) {
+			throw new IllegalArgumentException("There is no such Shift!");
+		}
+		ShiftDto shiftDto = new ShiftDto(s.getWorkingDay(),s.getStartTime(),s.getEndTime(),s.getShiftId());
+		return shiftDto;
+	}
+	
+	/**
+	 * Helper Method to convert a Customer to a Customer Dto
+	 * @author Zi Chao
+	 * @param user
+	 * @return CustomerDto
+	 */
+	private CustomerDto convertToDto(Customer customer) {
+		if (customer == null) {
+			throw new IllegalArgumentException("The provided customer does not exist.");
+		}
+		CustomerDto customerDto = new CustomerDto(customer.getFirstName(), customer.getLastName(), customer.getAccountId(), customer.getPassword(), customer.getEmail(), customer.getIsVerified(), customer.getIsLocal(), customer.getAccountBalance());
+	
+		return customerDto;
+	}
+}
--- LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/LibrarianService.java
+++ LibrarySystem_Backend/src/main/java/ca/mcgill/ecse321/librarysystem/service/LibrarianService.java
@@ -1,8 +1,5 @@
 package ca.mcgill.ecse321.librarysystem.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,8 +8,6 @@ import ca.mcgill.ecse321.librarysystem.dao.CustomerRepository;
 import ca.mcgill.ecse321.librarysystem.dao.LibrarianRepository;
 import ca.mcgill.ecse321.librarysystem.dao.WeeklyScheduleRepository;
 import ca.mcgill.ecse321.librarysystem.model.Customer;
-import ca.mcgill.ecse321.librarysystem.model.Librarian;
-import ca.mcgill.ecse321.librarysystem.model.WeeklySchedule;
 
 @Service
 public class LibrarianService {
@@ -21,138 +16,28 @@ public class LibrarianService {
 	@Autowired
 	WeeklyScheduleRepository weeklyScheduleRepository;
 	@Autowired
-  CustomerRepository customerRepository;
-	
-	@Transactional
-	public Librarian createLibrarian(String firstName, String lastName, String password, int newScheduleId) {
-		// Check for empty or null fields
-		if (firstName == null || firstName.trim().length() == 0) {
-			throw new IllegalArgumentException("First name cannot be empty.");
-		}
-		if (lastName == null || lastName.trim().length() == 0) {
-			throw new IllegalArgumentException("Last name cannot be empty.");
-		}
-		if (password == null || password.trim().length() == 0) {
-			throw new IllegalArgumentException("Password cannot be empty.");
-		}
-		
-		// Check if weeklySchedule exists
-		if (!weeklyScheduleRepository.existsByWeeklyScheduleId(newScheduleId)) {
-			throw new IllegalArgumentException("Weekly schedule with provided id could not be found in the database");
-		}
-		
-		Librarian librarian = new Librarian();
-		librarian.setFirstName(firstName);
-		librarian.setLastName(lastName);
-		librarian.setPassword(password);
-		librarian.setLibrarianSchedule(weeklyScheduleRepository.findByWeeklyScheduleId(newScheduleId));
-		librarianRepository.save(librarian);
-		return librarian;
-	}
-	
-	@Transactional
-	public Librarian assignWeeklySchedule(int id, int newScheduleId) {
-		// check if weekly schedule exists
-		if (!weeklyScheduleRepository.existsByWeeklyScheduleId(newScheduleId)) {
-			throw new IllegalArgumentException("Weekly schedule with provided id could not be found in the database");
-		}
-		// check if librarian exists
-		if (!librarianRepository.existsByAccountId(id)) {
-			throw new IllegalArgumentException("Librarian with provided account id could not found.");
-		}
-		
-		Librarian librarian = librarianRepository.findByAccountId(id);
-		librarian.setLibrarianSchedule(weeklyScheduleRepository.findByWeeklyScheduleId(newScheduleId));
-		librarianRepository.save(librarian);
-		return librarian;
-	}
-	
-	@Transactional
-	public Librarian deleteLibrarian(int accountId) {
-		// check if librarian exists
-		if (!librarianRepository.existsByAccountId(accountId)) {
-			throw new IllegalArgumentException("Librarian account to delete could not be found with provided id");
-		}
-		
-		Librarian librarian = librarianRepository.findByAccountId(accountId);
-		librarianRepository.delete(librarian);
-		return librarian;
-	}
-	
-	@Transactional
-	public List<Librarian> deleteAllLibrarians() {
-		List<Librarian> librarians = toList(librarianRepository.findAll());
-		librarianRepository.deleteAll();
-		return librarians;
-	}
-	
-	@Transactional
-	public Librarian getLibrarianByAccountId(int id) {
-		// check if librarian with provided id exists
-		if (!librarianRepository.existsByAccountId(id)) {
-			throw new IllegalArgumentException("Librarian with provided account id could not found.");
-		}
-		
-		Librarian librarian = librarianRepository.findByAccountId(id);
-		return librarian;
-	}
-	
-	@Transactional
-	public Librarian getLibrarianByNames(String firstName, String lastName) {
-		// check if librarian with first name and last name exists
-		if (!librarianRepository.existsByFirstNameAndLastName(firstName, lastName)) {
-			throw new IllegalArgumentException("Librarian with provided first name and last name could not be found.");
-		}
-		
-		Librarian librarian = librarianRepository.findByFirstNameAndLastName(firstName, lastName);
-		return librarian;
-	}
-	
-	@Transactional
-	public Librarian getLibrarianByScheduleId(int scheduleId) {
-		// Check if weekly schedule exists
-		if (!weeklyScheduleRepository.existsByWeeklyScheduleId(scheduleId)) {
-			throw new IllegalArgumentException("Provided weekly schedule could not be found in the database");
-		}
-		
-		WeeklySchedule schedule = weeklyScheduleRepository.findByWeeklyScheduleId(scheduleId);
-		Librarian librarian = librarianRepository.findByLibrarianSchedule(schedule);
-		return librarian;
-	}
-	
-	@Transactional
-	public List<Librarian> getAllLibrarians() {
-		return toList(librarianRepository.findAll());
-	}
+    CustomerRepository customerRepository;
 
 	@Transactional
-  public Customer updateCustomer(int id, boolean newIsVerified, boolean newIsLocal, int newBalance) {
-    if (String.valueOf(id).length() == 0) {
-      throw new IllegalArgumentException("Customer id cannot be empty.");
-    } 
-    else {
-      Customer customer = customerRepository.findCustomerByAccountId(id);
-      if (customer == null) {
-        throw new IllegalArgumentException("The customer with provided id cannot be found.");
-			}
-			if (String.valueOf(newBalance).length() == 0 || newBalance < 0) {
-				throw new IllegalArgumentException("The provided account balance cannot be empty or negative.");
-			}
+    public Customer updateCustomer(int id, boolean newIsVerified, boolean newIsLocal, int newBalance) {
+      if (String.valueOf(id).length() == 0) {
+        throw new IllegalArgumentException("Customer id cannot be empty.");
+      } 
       else {
-        customer.setIsVerified(newIsVerified);
-        customer.setIsLocal(newIsLocal);
-				customer.setAccountBalance(newBalance);
-        customerRepository.save(customer);
-        return customer;
+        Customer customer = customerRepository.findCustomerByAccountId(id);
+        if (customer == null) {
+          throw new IllegalArgumentException("The customer with provided id cannot be found.");
+		}
+		if (String.valueOf(newBalance).length() == 0 || newBalance < 0) {
+			throw new IllegalArgumentException("The provided account balance cannot be empty or negative.");
+		}
+        else {
+          customer.setIsVerified(newIsVerified);
+          customer.setIsLocal(newIsLocal);
+		  customer.setAccountBalance(newBalance);
+          customerRepository.save(customer);
+          return customer;
+        }
       }
     }
-  }
-	
-	private <T> List<T> toList(Iterable<T> iterable){
-		List<T> resultList = new ArrayList<T>();
-		for (T t : iterable) {
-			resultList.add(t);
-		}
-		return resultList;
-	}
 }
