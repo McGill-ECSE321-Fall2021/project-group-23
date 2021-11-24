d6fce32795ebab536abd92cc9118462740fab9e3
Abdoou213 <78567980+Abdoou213@users.noreply.github.com>
2021-11-24 13:37:08 -0500

    login and signup components

---
+++ LibrarySystem-Frontend/src/components/Signup.vue
@@ -0,0 +1,44 @@
+<template>
+<div id="Signup">
+    <table>
+      <h2>Sign Up</h2>
+      <tr>
+          <td>
+              <input type="text" placeholder="First name">
+          </td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Last name">
+          </td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Address">
+          </td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Email">
+          </td>
+      </tr>
+      <tr>
+          <td>
+              <input type="text" placeholder="Password">
+          </td>
+      </tr>
+      <tr>
+          <td>
+              <button>Sign Up</button>
+          </td>
+      <tr>  
+    </table>
+    <p>
+        <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+<script>
+</script>
+<style>
+</style>
--- LibrarySystem-Frontend/src/router/index.js
+++ LibrarySystem-Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+import Signup from '@/components/Signup'
 
 Vue.use(Router)
 
@@ -10,6 +12,18 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }
+    ,
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    }
+    ,
+    {
+      path: '/Signup',
+      name: 'Signup',
+      component: Signup
     }
   ]
 })
