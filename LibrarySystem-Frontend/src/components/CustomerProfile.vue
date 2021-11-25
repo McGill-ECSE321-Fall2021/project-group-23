52645d6598ed6fe2b73d6620737d0dc0cca3220d
Bashar Eskandar <basharo9999@hotmail.com>
2021-11-25 18:56:25 -0500

    user profile components

---
+++ LibrarySystem-Frontend/src/components/CustomerProfile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="customer-profile">
+      <div id="header">
+        <h2>User Profile - {{accountType}}</h2>
+        <h2>{{username}}</h2>
+        <hr>
+      </div>
+      <div id="body">
+          <h4>Account ID : {{id}}</h4>
+          <h4>Email      : {{email}}</h4>
+          <h4>Address    : {{address}}</h4>
+          <h4>Password   : {{password}}</h4>
+      </div>
+  </div>
+</template>
+<script>
+//import toadd from './toadd.vue'
+/*export default{
+    components: {
+        toadd
+    }
+}*/
+export default {
+  props: ["username", "id", "address", "email", "password", "accountType"],
+};
+</script>
+<style>
+</style>
---
+++ LibrarySystem-Frontend/src/components/Profile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="profile">
+        <customer-profile username='habibi' password='lol123' id="12345" address="lol Street" email=habibi@lol.com  accountType="habibi type" />
+    </div>
+</template>
+<script>
+import CustomerProfile from "@/components/CustomerProfile.vue"
+export default{
+    components : {
+        CustomerProfile
+    }
+}
+</script>
+<style>
+    
+</style>
