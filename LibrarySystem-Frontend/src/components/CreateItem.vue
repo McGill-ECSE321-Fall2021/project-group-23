c8f8f0b75d52756c12f2455ea9fa06e87be49b4d
JoelLussier <71228556+JoelLussier@users.noreply.github.com>
2021-11-25 00:47:45 -0500

    Format for Create Item

---
+++ LibrarySystem-Frontend/src/components/CreateItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="CreateItem">
+    <table>
+      <h2>Create Item</h2>
+      <tr>
+        <td>
+          <input type="text" placeholder="Title" />
+        </td>
+        <td>
+          ​<select>
+            ​<option disabled selected value="">Select Type</option>
+            <option >Book</option>
+            <option >Movie</option>
+            <option >Album</option>
+            <option >Newspapers</option>
+            <option >Archive</option>
+          </select>
+        </td>
+      </tr>
+      <tr>
+          <button>Create Item</button>
+      </tr>
+    </table>
+    <p>
+        <span style="color:red">Error: Message text comes here</span>
+    </p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
