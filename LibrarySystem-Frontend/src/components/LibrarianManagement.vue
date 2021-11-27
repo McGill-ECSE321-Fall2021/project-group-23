10db35d16a1c8a1d7bb58ebe28e4a8ecc7f3d220
Capt0bvious <abiola.olaniyan15@gmail.com>
2021-11-27 17:31:18 -0500

    Database will initialize with head librarian

--- LibrarySystem-Frontend/src/components/LibrarianManagement.vue
+++ LibrarySystem-Frontend/src/components/LibrarianManagement.vue
@@ -23,28 +23,64 @@
                     <td>
                         <input type="password" id="libpassword">
                     </td>
+                </tr>
+                <tr>
+                    <th>Day of Shift</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                </tr>
+                <tr>
                     <td>
-                        <button>Add New Librarian</button>
+                        <select name="Day of Week" id="day" v-model="newShift.day">
+                            <option value="Monday">Monday</option>
+                            <option value="Tuesday">Tuesday</option>
+                            <option value="Wednesday">Wednesday</option>
+                            <option value="Thursday">Thursday</option>
+                            <option value="Friday">Friday</option>
+                            <option value="Saturday">Saturday</option>
+                            <option value="Sunday">Sunday</option>
+                        </select>
+                    </td>
+                    <td>
+                        <input type="time" v-model="newShift.startTime" id="starttime">
+                    </td>
+                    <td>
+                        <input type="time" v-model="newShift.endTime" id="endtime">
                     </td>
                 </tr>
+                <tr>
+                    <td></td>
+                    <td> <button v-on:click="createShift(newShift.day, newShift.startTime, newShift.endTime)">Add Shift to Table</button> </td>
+                </tr>
             </table>
+            <span v-if="errorShift" style="color:red"> {{errorShift}} </span>
+        <h3 style="text-align:left">Shifts to Assign</h3>
+            <table>
+                <tr>
+                    <td> <th style="padding: 20px">Day of Week</th> </td>
+                    <td> <th style="padding: 20px">Start Time</th> </td>
+                    <td> <th style="padding: 20px">End Time</th> </td>
+                    <td> <th style="padding: 20px">Shift ID</th> </td>
+                </tr>
+                <tr v-for="shift in shifts" :key=shift>
+                    <td> {{shift.dayOfWeek}} </td>
+                    <td> {{shift.startTime}} </td>
+                    <td> {{shift.endTime}} </td>
+                    <td> {{shift.shiftId}} </td>
+                </tr>
+            </table> 
         <hr>
         <h2 style="text-align:left">Current Librarians</h2>
             <table>
                 <tr>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Librarian ID</th>
+                    <td> <th style="padding: 20px">First Name</th> </td>
+                    <td> <th style="padding: 20px">Last Name</th> </td>
+                    <td> <th style="padding: 20px">Librarian ID</th> </td>
                 </tr>
-                <tr>
-                    <td>Filler Cell</td>
-                    <td>Make Dynamic</td>
-                    <td>Later</td>
-                </tr>
-                <tr>
-                    <td>Bla</td>
-                    <td>BlaBla</td>
-                    <td>BlaBlaBla</td>
+                <tr v-for="librarian in librarians" :key=librarian>
+                    <td> {{librarian.fName}} </td>
+                    <td> {{librarian.lName}} </td>
+                    <td> {{librarian.libId}} </td>
                 </tr>
                 <tr>
                     <td>
@@ -61,9 +97,7 @@
     </div>    
 </template>
 
-<script>
-
-</script>
+<script src="./librarianManager.js"></script>
 
 <style>
 </style>
--- LibrarySystem-Frontend/src/components/librarianManager.js
+++ LibrarySystem-Frontend/src/components/librarianManager.js
@@ -16,27 +16,84 @@ var AXIOS = axios.create({
     baseURL: backendUrl
 })
 
+// Data transfer objects for Shift and Librarian
+function LibrarianDto (id, firstName, lastName, password) {
+    this.libId = id
+    this.fName = firstName
+    this.lName = lastName
+    this.pass = password
+}
+
+function ShiftDto(day, startTime, endTime, id) {
+    this.dayOfWeek = day
+    this.start = startTime
+    this.end = endTime
+    this.shiftId = id
+}
+
 export default {
     name: 'librarianmanagement',
     data () {
         return {
             librarians: [],
+            shifts: [],
             newLibrarian: '',
+            newShift: {
+                day: '',
+                startTime: '',
+                endTime: '',
+                id: ''
+            },
             selectedLibrarian: '',
+            selectedShift: '',
             errorLibrarian: '',
+            errorShift: '',
             response: []
         }
     },
     created: function () {
+        // // Test data
+        // const p1 = new LibrarianDto(12, 'Mac', 'Donald', 'ummm1')
+        // this.librarians = [p1]
+
         // Initialize librarians from backend
         AXIOS.get('/getAllLibrarians')
         .then(response => {
             this.librarians = response.data
         })
+        .catch(e => {
+            this.errorLibrarian = e
+        })
+        // Initialize shifts
+        AXIOS.get('/getAllShifts')
+        .then(response => {
+            this.shifts = response.data
+        })
+        .catch(e => {
+            this.errorShift = e
+        })
     },
     methods: {
         createLibrarian: function (librarianFName, librarianLName, librarianPass) {
-            AXIOS.post('/create')
+            AXIOS.post('/createLibrarian/'.concat(librarianFName + '/' + librarianLName + '/' + librarianPass))
+        },
+        createShift: function (day, start, end) {
+            AXIOS.post('/createShift', {}, {
+                params: {
+                    dayOfWeek: day,
+                    startTime: start,
+                    endTime: end
+                }
+            })
+            .then(response => {
+                this.shifts.push(response.data)
+                this.errorShift = ''
+            })
+            .catch(e => {
+                var errorMsg = e.response.data.message
+                console.log(errorMsg)
+                this.errorShift = errorMsg
+            })
         }
     }
 }
