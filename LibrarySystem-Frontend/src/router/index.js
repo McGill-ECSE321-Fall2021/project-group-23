f0a41158c4cfec614a59982c52ce371d8e715915
JoelLussier <71228556+JoelLussier@users.noreply.github.com>
2021-11-26 23:51:57 -0500

    Imlemented View Items

--- LibrarySystem-Frontend/src/router/index.js
+++ LibrarySystem-Frontend/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
 import Signup from '@/components/Signup'
+import CreateItem from '@/components/CreateItem.vue'
+import ViewItems from '@/components/ViewItems.vue'
 
 Vue.use(Router)
 
@@ -25,5 +27,18 @@ export default new Router({
       name: 'Signup',
       component: Signup
     }
+    ,
+    {
+      path: '/CreateItem',
+      name: 'CreateItem',
+      component: CreateItem
+    }
+    ,
+    {
+      path: '/ViewItems',
+      name: 'ViewItems',
+      component: ViewItems
+    }
+    ,
   ]
 })
