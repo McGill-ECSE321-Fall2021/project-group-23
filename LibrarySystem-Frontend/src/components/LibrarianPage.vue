3213a73f8e052cf5c4e57818708f856d231ed839
Eric Zhang <eric.zhang3@mail.mcgill.ca>
2021-11-29 21:10:28 -0500

    librarian page

---
+++ LibrarySystem-Frontend/src/components/LibrarianPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="hire/fire-librarian" class="container-fluid">
+        <h1 style="text-align:left">Head Librarian Management</h1>
+        <div>
+            <button class="btn btn-light" v-on:click="customerManagement()">Customer management</button>
+            <button class="btn btn-light" v-on:click="viewShifts()">View Shifts</button>
+            <button class="btn btn-light" v-on:click="createItem()">Item Management</button>
+            <button class="btn btn-light" v-on:click="goToProfile()">Profile</button>
+        </div>
+
+        <hr>
+    </div>    
+</template>
+
+<script src="./LibrarianPage.js"></script>
+
+<style>
+
+</style>
