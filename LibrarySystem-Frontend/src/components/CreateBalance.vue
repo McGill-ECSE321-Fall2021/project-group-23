c4a79dde2384cf63c87d9c43d89ef7172a5f887c
zichzhang <zi.c.zhang@mail.mcgill.ca>
2021-11-30 21:58:59 -0500

    CreateBalance

---
+++ LibrarySystem-Frontend/src/components/CreateBalance.vue
@@ -0,0 +1,85 @@
+<template>
+  <body>
+    <div class="balance">
+      <h1>Balance: 25.00$</h1>
+    </div>
+    <div class="container">
+      <div class="payment">
+        <section class="row options first">
+          <div class="item">
+            <label class="label">
+              <input class="radio" type="radio" name="channel" />
+              <span class="radioInput"></span><span>PayPal</span>
+            </label>
+          </div>
+        </section>
+        <section class="row options no-border-bottom">
+          <div class="item">
+            <label class="label">
+              <input class="radio" type="radio" name="channel" checked />
+              <span class="radioInput"></span><span>Debit/Credit card</span>
+            </label>
+          </div>
+        </section>
+        <section class="row blanks">
+          <div class="col">
+            <div class="item">
+              <span class="title">Card number</span>
+              <input
+                class="blank card-number"
+                placeholder="XXXX XXXX XXXX XXXX"
+                onFocus="this.value = ''"
+                onkeyup="if (this.value.length === 4 || this.value.length === 4 * 2 + 1 || this.value.length === 4 * 3 + 2) { this.value += ' ' }"
+                maxlength="19"
+                required
+              />
+            </div>
+          </div>
+        </section>
+        <section class="row blanks">
+          <div class="col">
+            <div class="item">
+              <span class="title">Cardholder Name</span>
+              <input
+                class="blank cardholder-name"
+                placeholder="John Doe"
+                required
+              />
+            </div>
+          </div>
+        </section>
+        <section class="row blanks last">
+          <div class="col">
+            <div class="item">
+              <span class="title">Expiry</span>
+              <input
+                class="blank"
+                placeholder="MM/YY"
+                onkeyup="if (this.value.length === 2) { this.value += '/' }"
+                maxlength="5"
+                required
+              />
+            </div>
+          </div>
+          <div class="col">
+            <div class="item">
+              <span class="title">CVC</span>
+              <input
+                class="blank"
+                placeholder="123"
+                maxlength="3"
+                required
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+      <button class="btn btn-light" type="button" onclick="location.href='https://www.youtube.com/watch?v=X_8Nh5XfRw0';">
+        Pay Balance Amount Due
+      </button>
+    </div>
+  </body>
+</template>
+<style scoped>
+  h1 {text-align: center;}
+</style>
